0dbff13c8bc513befd87b5cbfeea7bfa076c8406
kawa0x0A <t.kawai@aiqveone.co.jp>
2025-08-07 10:43:51 +0900

    Update products.tsx

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -12,8 +12,6 @@ type ProductProps = {
 };
 
 export default function Products({ products }: ProductProps) {
-    const [showMessage] = useState(false);
-
     const { addToCart } = useCart();
     const [showModal, setShowModal] = useState(false)
     const [selectedProduct, setSelectedProduct] = useState<FakeProductType>()
@@ -33,12 +31,6 @@ export default function Products({ products }: ProductProps) {
 
     return (
         <>
-            {showMessage && (
-                <div className="fixed top-4 right-4 bg-green-500 text-white px-4 py-2 rounded shadow-lg z-50 transition">
-                商品をカートに追加しました
-                </div>
-            )}
-
             <h1 className="text-2xl font-bold mb-4">商品一覧</h1>
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {products.map((product) => (
