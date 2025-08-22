b011810cc83f236720b85d19c0f4fedca2818ee8
kawa0x0A <103975497+TakahiroKawai@users.noreply.github.com>
2025-08-22 18:08:08 +0900

    ローディング表示を追加

---
+++ src/app/orders/loading.tsx
@@ -0,0 +1,5 @@
+import LoadingText from "@/components/loadingtext";
+
+export default function Loading() {
+  return <LoadingText />;
+}
---
+++ src/components/loadingtext.tsx
@@ -0,0 +1,7 @@
+export default function LoadingText() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p className="text-lg font-semibold animate-pulse">読み込み中です...</p>
+    </div>
+  );
+}
