e0139e2108e7ab0e6ea3ba157f16ac3e033970bf
kawaitakahiro <t.kawai@aiqveone.co.jp>
2025-10-15 20:04:36 +0900

    ログイン時にユーザー情報をデータベースに追加する機能を追加

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import prisma from "@/script/prismaclient";
 
 // 本来はセキュリティのためにシークレットなどはソースコードに直接記述しないが今回は勉強用サイトとして動作しやすさを優先するためにソースコードに直接記述する
 export const handler = NextAuth({
@@ -12,6 +13,22 @@ export const handler = NextAuth({
   secret: 'Qdl03y8W7kA6dGD1b4xATFwvXQDHk7DN2pdCsBuzFwU=',
   session: {
     strategy: "jwt",
+  },
+  callbacks: {
+    async signIn({ user }) {
+      // ユーザーがDBにいなければ追加
+      const exist = await prisma.user.findUnique({ where: { email: user.email! } });
+      if (!exist) {
+        await prisma.user.create({
+          data: {
+            name: user.name ?? "",
+            email: user.email!,
+            password: "",
+          },
+        });
+      }
+      return true;
+    },
   },
 });
 
